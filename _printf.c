b1ac7889a43bc6a7eeb27d7e3db135264c44f8d6
Tofik Gashaw <gashawtofik@gmail.com>
2022-04-19 05:50:52 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,6 +1,10 @@
 #include "main.h"
 
-
+/**
+ * _printf - prints formatted data to stdout
+ * @format: string that contains the format to print
+ * Return: number of characters written
+ */
 int _printf(const char *format, ...)
 {
 	va_list ap;
