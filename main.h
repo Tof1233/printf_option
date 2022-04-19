34d3508929b678e059b61dc550d17a569fe7526c
Tofik Gashaw <gashawtofik@gmail.com>
2022-04-19 09:43:03 +0300

    Update main.h

--- main.h
+++ main.h
@@ -2,18 +2,14 @@
 #define PRINTF_H
 
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 #include <stdarg.h>
-/**
- * struct printHandler - struct to choose the right function depending
- * on the format specifier passed to _printf()
- * @c: format specifier
- * @f: pointer to the correct printing function
- */
+#include <limits.h>
 
-int _printf(const char *fmt, ...);  
-unsigned int print_string(va_list l);
-unsigned int print_char(va_list l);
-unsigned int _putchar(char c);
-int _strcmp(const char *s1, const char *s2); 
+void _sprintf(const char *str);
+int _printf(const char *format, ...);
+void _count(int *ptr, char *str);
+
 
 #endif
