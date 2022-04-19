00cdaa1d0a170be71bc720017a2b41c238782d1b
Tofik Gashaw <gashawtofik@gmail.com>
2022-04-19 05:17:00 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,28 +1,15 @@
-#ifndef PRINT_F
-#define PRINT_F
+#ifndef PRINTF_H
+#define PRINTF_H
 
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 #include <stdarg.h>
+#include <limits.h>
 
-/**
-* struct convert - defines a structure for symbols and functions
-*
-* @sym: The operator
-* @f: The function associated
-*/
-struct convert
-{
-	char *sym;
-	int (*f)(va_list);
-};
-typedef struct convert conver_t;
-
-/*Main functions*/
-int parser(const char *format, conver_t f_list[], va_list arg_list);
+void _sprintf(const char *str);
 int _printf(const char *format, ...);
-int print_char(va_list); 
-int print_string(va_list); 
-int print_percent(va_list); 
+void _count(int *ptr, char *str);
+
 
 #endif
