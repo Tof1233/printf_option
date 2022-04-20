a5bc57020403c692888478cea11d5bf0f18a3df0
Tofik Gashaw <gashawtofik@gmail.com>
2022-04-20 11:59:44 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,15 +1,10 @@
 #ifndef PRINTF_H
 #define PRINTF_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include <limits.h>
 
-void _sprintf(const char *str);
+
+
 int _printf(const char *format, ...);
-void _count(int *ptr, char *str);
 unsigned int _putchar(char c);
 
 #endif
