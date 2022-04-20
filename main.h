9560737d4ae4e75545fc950d3d76975b4e33df7e
Tofik Gashaw <gashawtofik@gmail.com>
2022-04-20 12:02:56 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,7 +1,11 @@
 #ifndef PRINTF_H
 #define PRINTF_H
 
-
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
 
 
 int _printf(const char *format, ...);
